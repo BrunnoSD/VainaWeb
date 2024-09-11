69ff8338d00ab28e543a88f9f342a3984acf9a28
Brunno Soares <166448074+BrunnoSD@users.noreply.github.com>
2024-09-11 20:52:03 -0300

    just a addendum

--- JavaScript/introducaoJs/script.js
+++ JavaScript/introducaoJs/script.js
@@ -8,8 +8,8 @@ comentário de varias linhas
 // Declarando variaveis: var, let e const
 
 // variavel + nome da variavel + = + valor;
-//  criando nomes de variaveis:
-//  não podemos usar nomes com espaços
+// criando nomes de variaveis:
+// não podemos usar nomes com espaços
 //podemos usar camelCase - escrever duas ou mais palavras juntas, primeira palavra vai iniciar com a letra minuscula, seguida das outras palavras com a com primeira letra maiuscula
 //case sensitive # letras minusculas de letras maiusculas
 
@@ -18,13 +18,16 @@ comentário de varias linhas
 // string "texto"
 
 // não podemos reescrever, mas podemos reatribuir valor:
+
 let nomeDoAluno = "Luan Vieira";
 nomeDoAluno = "Alexandra";
 nomeDoAluno = "Gislane";
 nomeDoAluno = "Stefany";
 console.log(nomeDoAluno);
 
-// var vc consegue reescrever e tbm reatribuir
+// var você consegue reescrever e também reatribuir valores
+// não é mais tão usado atualmente, foi substituído por let
+
 var facilitador = "Kleber";
 var facilitador = "Débora";
 facilitador = "Fellipe";
